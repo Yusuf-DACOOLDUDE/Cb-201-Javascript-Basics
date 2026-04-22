46ff64832be815015e41a7c8bfaf0669e52e69cf
Yusuf-DACOOLDUDE <yusuf.stockman@codedbykids.org>
2026-04-22 23:00:49 +0000

    done dis jawn

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-let name = "Yusuf";
-console.log(name);
+let cost = 0
+let funds = 100
+
+let item1 = "fries"
+let price1 = 0.5
+
+let item2 = "chicken"
+let price2 = 24
+
+let item3 = "beef"
+let price3 = 45
+
+let item4 = "seafood"
+let price4 = 75
+
+cost = price1 + price2 + price3 + price4
+console.log(cost)
+funds -= cost
+console.log(funds)
+console.log(funds >= cost)
